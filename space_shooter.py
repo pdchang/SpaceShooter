ade2b504a3d548a890690f6fa85c2816b8a5f36f
Philip Chang <pdchang1@gmail.com>
2020-01-15 19:32:23 -0500

    fleet made

--- space_shooter.py
+++ space_shooter.py
@@ -29,18 +29,26 @@ class SpaceShooter:
     
     def _create_fleet(self):
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
 
+        ship_height = self.ship.rect.height
+        available_space_y = self.settings.screen_height - (3 * alien_height) - ship_height
+        number_alien_rows = available_space_y // (2 * alien_height)
+
+    
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
+        
+        for row in range(number_alien_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row)
 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
-
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row):
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
         alien.x = alien.rect.width + 2 * alien.rect.width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row
         self.aliens.add(alien)
 
     def run_game(self):
